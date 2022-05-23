28a1099f04650ce903ee41b3b18dbc8e6b2a3b90
Furkan Üzüm <63475692+furkanzm@users.noreply.github.com>
2022-05-23 14:06:21 +0300

    Update systeminterface.cpp

--- systeminterface.cpp
+++ systeminterface.cpp
@@ -8,57 +8,34 @@ SystemInterface::~SystemInterface()
 {
 }
 
-void SystemInterface::add(const std::string &buf)
+void SystemInterface::add(std::shared_ptr<Buf> buf)
 {
-    buffer.push(buf);
-    Notify();
+
+	buffer.push(buf);
+	Notify();
 }
 void SystemInterface::addOutput(SystemInterface *System)
 {
-    out.push_back(System);
+	outq.push_back(System);
 }
 
 int SystemInterface::Loop(NOTIFY_TYPE)
 {
-    std::unique_lock<std::mutex> lck(mutex_);
-    while (!buffer.empty()) {
-        std::string buf = process(buffer.front());
-        for (int i = 0; i < out.size(); ++i) {
-            out[i]->add(buf);
-        }
-        buffer.pop();
-    }
-    return 0;
-}
+	auto basePtr = std::shared_ptr<Buf>();
+	auto derivedPtr = std::shared_ptr<StringBuf>();
+	basePtr = std::static_pointer_cast<Buf>(derivedPtr);
+	auto downcastedPtr = std::dynamic_pointer_cast<StringBuf>(basePtr);
 
-SystemUpper::SystemUpper()
-{
+	if (downcastedPtr == std::shared_ptr<StringBuf>()) {
+		while (!buffer.empty()) {
+			auto buf = process(buffer.front());
+			for (int i = 0; i < outq.size(); ++i) {
+				std::unique_lock<std::mutex> lck(mutex_);
+				std::shared_ptr<Buf> bf = std::make_shared<StringBuf>(buf);
+				outq[i]->add(bf);
+			}
+			buffer.pop();
+		}
+	}
+	return 0;
 }
-
-SystemUpper::~SystemUpper()
-{
-}
-std::string SystemUpper::process(const std::string &buf)
-{
-    std::string strstr = buf;
-    std::transform(strstr.begin(), strstr.begin() + 3, strstr.begin(), ::toupper);
-    return strstr;
-}
-
-SystemLower::SystemLower()
-{
-}
-
-SystemLower::~SystemLower()
-{
-    
-}
-
-std::string SystemLower::process(const std::string &buf)
-{
-    
-    std::string strstr = buf;
-    std::transform(strstr.begin() + 3, strstr.end(), strstr.begin() + 3, ::tolower);
-    return strstr;
-}
-
