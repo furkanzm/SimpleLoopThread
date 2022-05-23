4743b45ed68c16d90e620c34293a0e8d707898af
Furkan Üzüm <63475692+furkanzm@users.noreply.github.com>
2022-05-23 14:04:55 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,42 +1,54 @@
+#include "buf.h"
 #include "simpleloopthread.h"
 #include "systeminterface.h"
+#include "systemlower.h"
+#include "systemupper.h"
 #include <iostream>
 class dum : public SystemInterface
 {
 public:
 	dum() {}
-	std::string process(const std::string &buf)
+	std::string process(std::shared_ptr<Buf> buf)
 	{
-		std::cout << buf << std::endl;
+		std::cout << "size: " << buf->size() << std::endl;
 		return " ";
 	}
 };
 
 int main()
 {
-
 	SystemUpper upper;
 	SystemLower lower;
 	dum dm;
-	upper.addOutput(&lower);
-	lower.addOutput(&dm);
+	//	upper.addOutput(&lower);
+	//	lower.addOutput(&dm);
+	lower.addOutput(&upper);
+	upper.addOutput(&dm);
 	dm.Start();
+	// std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 	upper.Start();
 	lower.Start();
+	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	std::cout << "\n";
 
-	std::this_thread::sleep_for(std::chrono::milliseconds(400));
-	upper.add("\nFURKAN uzum");
-	std::this_thread::sleep_for(std::chrono::milliseconds(400));
-    upper.add("aBCdefGehASdSd");
-    std::this_thread::sleep_for(std::chrono::milliseconds(400));
-    upper.add("hllLLh");
-    std::this_thread::sleep_for(std::chrono::milliseconds(400));
-    upper.add("aNkArA CankAYA");
-	upper.add("aNkArA CankAYA");
-	std::this_thread::sleep_for(std::chrono::milliseconds(400));
-	upper.add("YIRMI MAyıs");
-	std::this_thread::sleep_for(std::chrono::milliseconds(400));
-	upper.add("SpaRSe TecH");
+	std::shared_ptr<Buf> bf = std::make_shared<StringBuf>("FuRkAnUzUmADaSd");
+	lower.add(bf);
 	std::this_thread::sleep_for(std::chrono::milliseconds(1200));
+
+	std::shared_ptr<Buf> bg =
+		std::make_shared<StringBuf>("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+	lower.add(bg);
+	std::this_thread::sleep_for(std::chrono::milliseconds(1200));
+
+	std::shared_ptr<Buf> bl = std::make_shared<StringBuf>("YiRmIUcMaYıS2022");
+	lower.add(bl);
+	std::this_thread::sleep_for(std::chrono::milliseconds(1200));
+
+	std::shared_ptr<Buf> bc =
+		std::make_shared<StringBuf>("FFffffffffffFFFFFFffff");
+	lower.add(bc);
+	//	std::shared_ptr<Buf> bf(new StringBuf("ffffffffff"));
+	//    upper.add(bf);
+	std::this_thread::sleep_for(std::chrono::milliseconds(2000));
 	return 0;
 }
