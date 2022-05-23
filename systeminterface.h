7354502e5e87595f0693a9ba9e69a503d28ceee3
Furkan Üzüm <63475692+furkanzm@users.noreply.github.com>
2022-05-23 14:06:40 +0300

    Update systeminterface.h

--- systeminterface.h
+++ systeminterface.h
@@ -1,8 +1,11 @@
 #ifndef SYSTEMINTERFACE_H
 #define SYSTEMINTERFACE_H
 
-#include "simpleloopthread.h"   
+#include "buf.h"
+#include "simpleloopthread.h"
+#include "stringbuf.h"
 #include <iostream>
+#include <atomic>
 #include <queue>
 
 class SystemInterface : public SimpleLoopThread
@@ -10,32 +13,16 @@ class SystemInterface : public SimpleLoopThread
 public:
 	SystemInterface();
 	~SystemInterface();
-	void add(const std::string &buf);
+	void add(std::shared_ptr<Buf> buf);
 	void addOutput(SystemInterface *System);
-	virtual std::string process(const std::string &) = 0;
-	// calısacak fonksıyon processı cagıracak processten gelen verıyı out lara
-	// gonderecek
+	virtual std::string process(std::shared_ptr<Buf>) = 0;
+
 private:
 	int Loop(NOTIFY_TYPE nt) override;
-	std::vector<SystemInterface *> out;
-	std::queue<std::string> buffer;
+	std::vector<SystemInterface *> outq;
+	std::queue<std::shared_ptr<Buf>> buffer;
 	std::mutex mutex_;
+	bool flag;
 };
 
-class SystemUpper : public SystemInterface
-{
-public:
-	SystemUpper();
-	~SystemUpper();
-	std::string process(const std::string &buf) override;
-
-};
-
-class SystemLower : public SystemInterface
-{
-public:
-	SystemLower();
-	~SystemLower();
-	std::string process(const std::string &buf) override;
-};
 #endif // SYSTEMINTERFACE_H
