1fcb485449ed3aeb91c406942e060df983525bcf
Furkan Üzüm <63475692+furkanzm@users.noreply.github.com>
2022-05-24 13:45:06 +0300

    Create systeminterface.cpp

--- systeminterface.cpp
+++ systeminterface.cpp
@@ -10,32 +10,27 @@ SystemInterface::~SystemInterface()
 
 void SystemInterface::add(std::shared_ptr<Buf> buf)
 {
-
 	buffer.push(buf);
 	Notify();
 }
-void SystemInterface::addOutput(SystemInterface *System)
+
+void SystemInterface::addOutput(std::shared_ptr<SystemInterface> System)
 {
 	outq.push_back(System);
 }
 
 int SystemInterface::Loop(NOTIFY_TYPE)
 {
-	auto basePtr = std::shared_ptr<Buf>();
-	auto derivedPtr = std::shared_ptr<StringBuf>();
-	basePtr = std::static_pointer_cast<Buf>(derivedPtr);
-	auto downcastedPtr = std::dynamic_pointer_cast<StringBuf>(basePtr);
 
-	if (downcastedPtr == std::shared_ptr<StringBuf>()) {
-		while (!buffer.empty()) {
-			auto buf = process(buffer.front());
-			for (int i = 0; i < outq.size(); ++i) {
-				std::unique_lock<std::mutex> lck(mutex_);
-				std::shared_ptr<Buf> bf = std::make_shared<StringBuf>(buf);
-				outq[i]->add(bf);
-			}
-			buffer.pop();
+	while (!buffer.empty()) {
+
+		std::unique_lock<std::mutex> lck(mutex_);
+		std::shared_ptr<Buf> buf = buffer.front();
+		auto bufR = process(buf);
+		for (int i = 0; i < outq.size(); ++i) {
+			outq[i]->add(bufR);
 		}
+		buffer.pop();
 	}
 	return 0;
 }
